bd0aa6ea798516a3ea71d065e51fb346c365c682
Brad Kovach <bradkovach@gmail.com>
2021-11-19 16:00:15 -0700

    formatting changes from prettier

--- src/commandhandler.ts
+++ src/commandhandler.ts
@@ -1,11 +1,8 @@
-
 import { Message } from 'discord.js';
 import { ICommand } from './ICommand';
 import { query } from './mysql';
 import { UserRow } from './rows/UserRow';
 import { Methods } from './utils/methods';
-
-
 
 export const handleCmd = async (message: Message, prefix: string) => {
 	const args = message.content.slice(prefix.length).split(/ +/);
@@ -16,8 +13,6 @@ export const handleCmd = async (message: Message, prefix: string) => {
 			(cmd: ICommand) => cmd.aliases && cmd.aliases.includes(commandName)
 		);
 
-		console.log({command});
-
 	if (!command) return;
 	// Command doesnt exist
 	else if (!command.worksInDM && message.channel.type !== 'text')
@@ -26,21 +21,41 @@ export const handleCmd = async (message: Message, prefix: string) => {
 		return message.reply('That command only works in DMs!');
 
 	if (message.channel.type !== 'dm') await cacheMembers(message);
-	if (!(await query<UserRow[]>(`SELECT * FROM users WHERE userId = ${message.author.id}`)).length){
+	if (
+		(
+			await query<UserRow[]>(
+				`SELECT * FROM users WHERE userId = ${message.author.id}`
+			)
+		).length ===0
+	) {
 		let methods = new Methods();
 		await methods.createNewUser(message.author.id); // Create new account in database for user BEFORE executing a command.
 	}
 
-	const row = (await query<UserRow[]>(`SELECT * FROM users WHERE userId = ${message.author.id}`))[0];
+	const row = (
+		await query<UserRow[]>(
+			`SELECT * FROM users WHERE userId = ${message.author.id}`
+		)
+	)[0];
 
 	if (command.requirePartner && row.partnerId == 0)
 		return message.reply(
 			"A partner has not been chosen for you yet! Try again after you've been given a partner."
 		);
-	else if (command.modOnly && !message.member!.hasPermission('MANAGE_GUILD'))
-		return message.reply('You need the `MANAGE_SERVER` permission to run that command.');
-	else if (command.adminOnly && !message.client.sets.adminUsers.has(message.author.id))
-		return message.reply('You must be an admin of the bot to run that command.');
+	else if (
+		command.modOnly &&
+		!message.member!.hasPermission('MANAGE_GUILD')
+	)
+		return message.reply(
+			'You need the `MANAGE_SERVER` permission to run that command.'
+		);
+	else if (
+		command.adminOnly &&
+		!message.client.sets.adminUsers.has(message.author.id)
+	)
+		return message.reply(
+			'You must be an admin of the bot to run that command.'
+		);
 
 	try {
 		command.execute(message, args, prefix); // CALL COMMAND HERE
--- src/discord.d.ts
+++ src/discord.d.ts
@@ -1,15 +1,15 @@
-import { ICommand } from "./ICommand";
+import { ICommand } from './ICommand';
 
-declare module "discord.js" {
-    export interface Client {
-        commands: Collection<unknown, ICommand>
-        commandsUsed: number;
-        sets: { bannedUsers: Set<string>; adminUsers: Set<string> };
-        fullLockdown: boolean;
-    }
-    export interface Command {
-        name: string,
-        description: string,
-        execute: (message: Message, args: string[]) => Promise<any>
-    }
+declare module 'discord.js' {
+	export interface Client {
+		commands: Collection<unknown, ICommand>;
+		commandsUsed: number;
+		sets: { bannedUsers: Set<string>; adminUsers: Set<string> };
+		fullLockdown: boolean;
+	}
+	export interface Command {
+		name: string;
+		description: string;
+		execute: (message: Message, args: string[]) => Promise<any>;
+	}
 }
