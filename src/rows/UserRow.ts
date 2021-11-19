2e7e22cf87c759dca13696f3b152febce2c7aec5
Brad Kovach <bradkovach@gmail.com>
2021-11-19 15:58:18 -0700

    add address, tracking_number and received to UserRow

--- src/rows/UserRow.ts
+++ src/rows/UserRow.ts
@@ -1,6 +1,9 @@
 export interface UserRow {
-    userId: number;
-    exchangeId: number;
-    wishlist: string;
-    partnerId: number;
+	userId: number;
+	exchangeId: number;
+	wishlist: string;
+	address: string;
+	partnerId: number;
+	tracking_number: string;
+	received: number;
 }
