90a24089e11d4e94a26390ae731605df63cf2afa
Brad Kovach <bradkovach@gmail.com>
2021-11-19 15:59:11 -0700

    Improve "Welcome" message

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,21 @@
-import { Client, Collection, Message, MessageEmbed } from "discord.js";
+import {
+	Client,
+	Collection,
+	Message,
+	MessageEmbed,
+	MessageReaction,
+	PartialUser,
+	User,
+} from 'discord.js';
 import fs from 'fs';
-import { handleCmd } from "./commandhandler";
+import { handleCmd } from './commandhandler';
 import config from './config.json';
-import { query } from "./mysql";
-import { BanRow } from "./rows/BanRow";
-import { ExchangeRow } from "./rows/ExchangeRow";
-import { UserRow } from "./rows/UserRow";
-import { Methods } from "./utils/methods";
-import {sets} from './utils/sets';
+import { query } from './mysql';
+import { BanRow } from './rows/BanRow';
+import { ExchangeRow } from './rows/ExchangeRow';
+import { UserRow } from './rows/UserRow';
+import { Methods } from './utils/methods';
+import { sets } from './utils/sets';
 
 import path from 'path';
 
@@ -19,7 +27,7 @@ const client = new Client({
 	partials: ['MESSAGE', 'REACTION', 'GUILD_MEMBER', 'USER'],
 });
 
-client.sets = sets
+client.sets = sets;
 client.commands = new Collection();
 client.commandsUsed = 0;
 client.fullLockdown = true; // Will be disabled after bot starts up.
@@ -27,29 +35,20 @@ client.fullLockdown = true; // Will be disabled after bot starts up.
 const commandFiles = fs
 	.readdirSync('./dist/commands')
 	.filter((filename: string) => filename.endsWith('.js'));
-const adminCommands = fs
-	.readdirSync('./dist/commands/admin')
-	.filter((filename: string) => filename.endsWith('.js'));
 
-for (const file of adminCommands) {
-	commandFiles.push(`admin/${file}`);
-}
 for (const file of commandFiles) {
 	const filepath = path.resolve(process.cwd(), 'dist', 'commands', file);
-	console.log({filepath});
 	const command = require(filepath).default;
-	console.log({command})
 	client.commands.set(command.name, command);
 }
 
 client.on('message', (message: Message) => {
-	console.log('got message', message.content);
 	if (message.author.bot) return;
-	else if (client.fullLockdown) return console.log('[APP] Ignored message.');
+	else if (client.fullLockdown)
+		return console.log('[APP] Ignored message.');
 	else if (client.sets.bannedUsers.has(message.author.id)) return;
-	else if (!message.content.toLowerCase().startsWith(config.prefix)) return; // Ignore if message doesn't start with prefix.
-
-	console.log('passed if-else-if blocks');
+	else if (!message.content.toLowerCase().startsWith(config.prefix))
+		return; // Ignore if message doesn't start with prefix.
 
 	if (message.channel.type === 'dm') handleCmd(message, config.prefix);
 	else handleCmd(message, config.prefix);
@@ -61,12 +60,12 @@ client.on('error', (err: any) => {
 
 client.on('disconnect', (err: any) => {
 	console.log(err);
-	client.destroy()
+	client.destroy();
 	//.then(k=>client.login(config.botToken));
-	process.exit()
+	process.exit();
 });
 
-client.on('debug', (message:any) => {
+client.on('debug', (message: any) => {
 	if (config.debug) console.debug(message);
 });
 
@@ -78,10 +77,9 @@ client.on('ready', async () => {
 	client.user!.setActivity(config.prefix + 'help', { type: 'PLAYING' });
 
 	const bannedRows = await query<BanRow[]>(`SELECT * FROM banned`); // refreshes the list of banned users on startup
-	console.log({bannedRows});
-	bannedRows.forEach((bannedId:any) => {
+	bannedRows.forEach((bannedId) => {
 		if (bannedId.userId !== undefined && bannedId.userId !== null) {
-			client.sets.bannedUsers.add(bannedId.userId);
+			client.sets.bannedUsers.add(bannedId.userId.toString());
 		}
 	});
 
@@ -89,46 +87,118 @@ client.on('ready', async () => {
 	client.fullLockdown = false;
 });
 
-client.on('messageReactionAdd', async (reaction: any, user: any) => {
-	if (reaction.emoji.name !== '🎅') return;
+import wishlistCommand from './commands/setwishlist';
+import addressCommand from './commands/address';
+import leaveCommand from './commands/leave';
 
-	const exchangeId = reaction.message.id;
-	const exchange = (
-		await query<ExchangeRow[]>(`SELECT * FROM exchange WHERE exchangeId = ${exchangeId}`)
-	)[0];
+client.on(
+	'messageReactionAdd',
+	async (reaction: MessageReaction, user: User | PartialUser) => {
+		if (reaction.emoji.name !== '🎅') return;
 
-	// no exchange associated with message
-	if (!exchange) return;
-	// exchange already started
-	else if (exchange.started === 1) return;
-
-	let row = (await query<UserRow[]>(`SELECT * FROM users WHERE userId = ${user.id}`))[0];
-
-	if (!row) {
-		let methods = new Methods();
-		await methods.createNewUser(user.id);
-		row = (await query<UserRow[]>(`SELECT * FROM users WHERE userId = ${user.id}`))[0];
-	}
-
-	if (row.exchangeId === 0) {
-		await query(`UPDATE users SET exchangeId = ${exchangeId} WHERE userId = ${user.id}`);
-
-		const joinEmbed = new MessageEmbed()
-			.setDescription(
-				`__Successfully joined <@${exchange.creatorId}>'s Secret Santa!__\nI will let you know when names are drawn!`
+		const exchangeId = reaction.message.id;
+		const exchange = (
+			await query<ExchangeRow[]>(
+				`SELECT * FROM exchange WHERE exchangeId = ${exchangeId}`
 			)
-			.setColor(config.embeds_color);
+		)[0];
 
-		const recipient = await client.users.fetch(user.id);
+		// no exchange associated with message
+		if (!exchange) return;
+		// exchange already started
+		else if (exchange.started === 1) return;
 
-		recipient.send(joinEmbed);
+		let row = (
+			await query<UserRow[]>(
+				`SELECT * FROM users WHERE userId = ${user.id}`
+			)
+		)[0];
+
+		if (!row) {
+			let methods = new Methods();
+			await methods.createNewUser(user.id);
+			row = (
+				await query<UserRow[]>(
+					`SELECT * FROM users WHERE userId = ${user.id}`
+				)
+			)[0];
+		}
+
+		if (row.exchangeId === 0) {
+			await query(
+				`UPDATE users SET exchangeId = ${exchangeId} WHERE userId = ${user.id}`
+			);
+
+			let messageSections = {
+				[`**Welcome to <@${exchange.creatorId}>'s Secret Santa Exchange**`]:
+					[
+						'I will let you know when names are drawn!',
+						'Before names are drawn, you need to set your profile and your address.',
+						'If you do not set your profile and address, you will not be selected for the exchange.',
+						'Names should be drawn by 2021-11-30 at 9:00 PM EST.',
+					],
+				'**1. Create Your Profile**': [
+					`Before we can match you to another participant, you need to submit your profile.`,
+					`Your profile will be provided to your Secret Santa so they can send you a gift you'll like.`,
+					`**To set your profile, reply to me...**\n\`${config.prefix}${wishlistCommand.name} <profile about you>\``,
+				],
+				'*For example, to set your profile, you could reply...*': [
+					[
+						'```',
+						[
+							`${config.prefix}${wishlistCommand.name}`,
+							`I really like motorcycles, techno, and cooking.`,
+							`My favorite TV Show is Breaking Bad.`,
+							`My favorite food is Coconut Curry.`,
+							`I have one dog, named Ratchet, and one cat, named Clank.`,
+							`I like tinkering with electronics.`,
+						].join(' '),
+						'```',
+					].join('\n'),
+				],
+				'**2. Set Your Address**': [
+					`You need to provide a shipping address to participate in Secret Santa.`,
+					'If you provide your physical address, your Santa will see the complete address.',
+					'Use a work address or a PO Box if you do not want your real address to be shared',
+					`Please make sure your address contains your country.`,
+					`Due to pandemic embargoes, some countries are not accepting mail from other countries.`,
+					`**To set your address, reply to me...**\n\`${config.prefix}${addressCommand.name} <your address>\``,
+				],
+				'*For example, to set your address, you could reply...*': [
+					[
+						'```',
+						`${config.prefix}${addressCommand.name} Firstname Lastname; c/o @${user.username}; 1600 Pennsylvania Avenue, N.W; Washington, DC 20500; USA`,
+						'```',
+					].join('\n'),
+				],
+				'**Leaving/Quitting The Exchange**': [
+					`If you are unable to participate in the exchange, you can leave any time before names are matched by replying here with \`${config.prefix}${leaveCommand.name}\`.`,
+					`If you need to leave the Secret Santa exchange *after* names are drawn, please contact one of the admins listed here: ${config.adminUsers
+						.map((userId) => `<@${userId}>`)
+						.join(', ')}`,
+				],
+			};
+
+			let messageString = Object.keys(messageSections)
+				.map((title, idx, sections) => {
+					let strings = messageSections[title];
+					return `${title}\n${strings.join(' ').trim()}`;
+				})
+				.join('\n\n');
+
+			const recipient = await client.users.fetch(user.id);
+
+			recipient.send({ content: messageString });
+		}
 	}
-});
+);
 
 process.on('unhandledRejection', (reason, p) => {
 	console.error(
 		'[APP][' +
-			new Date().toLocaleString(undefined, { timeZone: 'America/New_York' }) +
+			new Date().toLocaleString(undefined, {
+				timeZone: 'America/New_York',
+			}) +
 			'] Unhandled Rejection: ',
 		reason
 	);
