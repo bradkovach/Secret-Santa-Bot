83a6ebad865add04a592c5ddc23c65ede22db8e4
Brad Kovach <bradkovach@gmail.com>
2021-11-19 15:59:55 -0700

    change mysql config to use .env file

--- src/mysql.ts
+++ src/mysql.ts
@@ -1,56 +1,26 @@
+import * as mysql from 'mysql';
+import { config } from 'dotenv';
 
-const config = require('./config.json');
+export interface SecretSantaConfig {
+	MYSQLDB_USER: string;
+	MYSQLDB_ROOT_PASSWORD: string;
+	MYSQLDB_DATABASE: string;
+	MYSQLDB_LOCAL_PORT: string;
+	MYSQLDB_DOCKER_PORT: string;
+	MYSQLDB_HOST: string;
+}
 
-import * as mysql from 'mysql'
+const resolvedConfig = config().parsed as unknown as SecretSantaConfig;
 
 export const db: mysql.Connection = mysql.createConnection({
-	host: config.sqlhostname,
-	user: config.sqluser,
-	password: config.sqlpass,
-	database: config.sqldatabase,
-	port: config.sqlport,
+	host: resolvedConfig.MYSQLDB_HOST,
+	user: resolvedConfig.MYSQLDB_USER,
+	password: resolvedConfig.MYSQLDB_ROOT_PASSWORD,
+	database: resolvedConfig.MYSQLDB_DATABASE,
+	port: parseInt(resolvedConfig.MYSQLDB_LOCAL_PORT, 10),
 	supportBigNumbers: true,
 	bigNumberStrings: false,
 	charset: 'utf8mb4',
-});
-
-
-db.connect((err: any) => {
-	if (err)
-		return console.log(
-			'[MYSQL] Could not connect to SQL database: ' +
-				err +
-				'\n\n\nYou probably need to create the secret_santa database, open "MYSQL Command Line Client" using windows search and run: CREATE DATABASE IF NOT EXISTS secret_santa;\n\n\n'
-		);
-
-	console.log('[MYSQL] MySQL Connected.');
-
-	// Create banned users table
-	db!.query(
-		'CREATE TABLE IF NOT EXISTS banned (userId bigint, reason VARCHAR(2048), date bigint) ENGINE = InnoDB',
-		(err: any, result: any) => {
-			if (err) return console.log(err);
-		}
-	);
-
-	// Create exchange table
-	db!.query(createExchangeSQL, (err: any, result: any) => {
-		if (err) return console.log(err);
-	});
-
-	// Create users table
-	db!.query(createUsersSQL, (err: any, result) => {
-		if (err) return console.log(err);
-	});
-});
-
-db.on('error', (err: { fatal: any; }) => {
-	if (err.fatal) {
-		console.log('[MYSQL] Fatal SQL error, attempting to reconnect.');
-		db!.end();
-		// connectSQL();
-		return;
-	}
 });
 
 const createUsersSQL = `
@@ -71,11 +41,48 @@ CREATE TABLE IF NOT EXISTS exchange (
     ENGINE = InnoDB
 `;
 
+const createBannedSQL = `CREATE TABLE IF NOT EXISTS banned (userId bigint, reason VARCHAR(2048), date bigint) ENGINE = InnoDB`;
+
+db.connect((err: any) => {
+	if (err)
+		return console.log(
+			'[MYSQL] Could not connect to SQL database: ' +
+				err +
+				'\n\n\nYou probably need to create the secret_santa database, open "MYSQL Command Line Client" using windows search and run: CREATE DATABASE IF NOT EXISTS secret_santa;\n\n\n'
+		);
+
+	console.log('[MYSQL] MySQL Connected.');
+
+	// Create banned users table
+	db!.query(createBannedSQL, (err: any, result: any) => {
+		if (err) return console.log(err);
+	});
+
+	// Create exchange table
+	db!.query(createExchangeSQL, (err: any, result: any) => {
+		if (err) return console.log(err);
+	});
+
+	// Create users table
+	db!.query(createUsersSQL, (err: any, result) => {
+		if (err) return console.log(err);
+	});
+});
+
+db.on('error', (err: { fatal: any }) => {
+	if (err.fatal) {
+		console.log('[MYSQL] Fatal SQL error, attempting to reconnect.');
+		db!.end();
+		// connectSQL();
+		return;
+	}
+});
+
 export function query<RowType>(sql: any, args?: any): Promise<RowType> {
 	return new Promise((resolve, reject) => {
 		db!.query(sql, args, (err: any, rows: unknown) => {
 			if (err) return reject(err);
-			
+
 			resolve(rows as RowType);
 		});
 	});
